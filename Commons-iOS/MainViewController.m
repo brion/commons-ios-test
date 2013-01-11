ad5dc368ac34a9cc6bc0aab93c3211a191397cf7
Karl Bautista <garlicfries@gmail.com>
2013-01-11 15:29:53 -0800

    Solution to commons-ios-test
    Completed the following:
    
    Core exercises:
    
    1) Detect login and upload errors and give feedback to the user.
    2) After uploading, display a button that opens the new file in Safari;
    use URL format 'http://test.wikipedia.org/wiki/File:' + filename
    3) Move i/o off the UI thread and provide visual progress feedback.
    
    Bonus exercises:
    
    Use correct naming style for UI field properties in controller classes.
    
    When pulling existing images from the gallery, retrieve the original
    file via asset URL and upload that instead of recompression from a
    UIImage

--- Commons-iOS/MainViewController.m
+++ Commons-iOS/MainViewController.m
@@ -9,12 +9,16 @@
 #import "MainViewController.h"
 #import "AppDelegate.h"
 #import "mwapi/MWApi.h"
+#import <Foundation/Foundation.h>
 
 @interface MainViewController ()
 @property (weak, nonatomic) AppDelegate *appDelegate;
 @end
 
 @implementation MainViewController
+NSString *uploadFileName = @"";
+NSURL *theAssetURL;
+UIImage *fullImage;
 
 - (void)viewDidLoad
 {
@@ -22,7 +26,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
 
     self.appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-
     if ([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera]) {
         // Camera is available
     } else {
@@ -31,6 +34,8 @@
         [self.TakePhotoButton setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
         self.TakePhotoButton.enabled = NO;
     }
+    _spinnerBackground.hidden = YES;
+    _viewSafari.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -80,28 +85,106 @@
 
 - (IBAction)pushedUploadFiles:(id)sender {
     NSLog(@"Upload ye files!");
-
+    _viewSafari.hidden = YES;
     NSString *username = self.appDelegate.username;
     NSString *password = self.appDelegate.password;
     NSString *desc = self.DescriptionTextView.text;
-    NSString *filename = [NSString stringWithFormat:@"Testfile %li.jpg", (long)[[NSDate date] timeIntervalSince1970]];
-    UIImage *image = self.imagePreview.image;
-    NSData *jpeg = UIImageJPEGRepresentation(image, 0.9);
-
-    NSLog(@"username: %@, password: %@, desc: %@, jpeg: %i bytes", username, password, desc, (int)(jpeg.length));
+    NSString *filename = [NSString stringWithFormat:@"Testfile_%li.png", (long)[[NSDate date] timeIntervalSince1970]];
+    uploadFileName = filename;
+    //Using AssetURL instead
+    //UIImage *image = self.imagePreview.image;
+    //NSData *jpeg = UIImageJPEGRepresentation(image, 0.9);
+    NSData *png = UIImagePNGRepresentation(fullImage);
+    
+    //NSData *jpeg = UIImagePNGRepresentation(fullImage);
+    
+    NSLog(@"username: %@, password: %@, desc: %@, png: %i bytes", username, password, desc, (int)(png.length));
     
     // hack hack hack
     // Upload the file
-    NSURL *url = [NSURL URLWithString:@"https://test.wikipedia.org/w/api.php"];
-    MWApi *mwapi = [[MWApi alloc] initWithApiUrl:url];
+    _spinnerBackground.hidden = NO;
+    _spinny.hidesWhenStopped = YES;
+    [_spinny startAnimating];
+    
+    dispatch_queue_t downloadQueue = dispatch_queue_create("upload", NULL);
+    dispatch_async(downloadQueue, ^{
+        NSURL *url = [NSURL URLWithString:@"https://test.wikipedia.org/w/api.php"];
+        MWApi *mwapi = [[MWApi alloc] initWithApiUrl:url];
+        
+        NSString *loginResult = [mwapi loginWithUsername:username andPassword:password withCookiePersistence:YES];
+        NSLog(@"login: %@", loginResult);
+        bool flag = true;
+        if (![loginResult isEqualToString:@"Success"]) {
+            [self resultError:@"Login Failed" andResult:loginResult];
+            flag = false;
+        }
+        else{
+            MWApiResult *uploadResult = [mwapi uploadFile:filename withFileData:png text: desc comment:desc];
+            NSLog(@"upload: %@", uploadResult.responseBody);
+            NSRange temp = [uploadResult.responseBody rangeOfString:@"Success"];
+            if (temp.location == NSNotFound) {
+                [self resultError:@"Upload Failed" andResult:uploadResult.responseBody];
+                flag = false;
+            }
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [_spinny stopAnimating];
+            _spinnerBackground.hidden = YES;
+            if(flag)
+                _viewSafari.hidden = NO;
+        });
+    });
+    dispatch_release(downloadQueue);
 
-    NSString *loginResult = [mwapi loginWithUsername:username andPassword:password withCookiePersistence:YES];
-    NSLog(@"login: %@", loginResult);
-
-    MWApiResult *uploadResult = [mwapi uploadFile:filename withFileData:jpeg text: desc comment:desc];
-    NSLog(@"upload: %@", uploadResult);
+   
     
     NSLog(@"done uploading...");
+}
+
+//Gets 
+- (void)getAsset
+{
+    ALAssetsLibrary *assetLibrary=[[ALAssetsLibrary alloc] init];
+    [assetLibrary assetForURL:theAssetURL resultBlock:^(ALAsset *asset) {
+        ALAssetRepresentation *rep = [asset defaultRepresentation];
+        Byte *buffer = (Byte*)malloc(rep.size);
+        NSUInteger buffered = [rep getBytes:buffer fromOffset:0.0 length:rep.size error:nil];
+        NSData *data = [NSData dataWithBytesNoCopy:buffer length:buffered freeWhenDone:YES];
+        fullImage = [UIImage imageWithData:data];
+    } failureBlock:^(NSError *err) {
+        NSLog(@"Error: %@",[err localizedDescription]);
+    }];
+}
+
+- (void)resultError:(NSString *)errorType andResult:(NSString *)result
+{
+    NSString *message;
+    if ([errorType isEqualToString:@"Login Failed"]) {
+        if ([result isEqualToString:@"NoName"]) {
+            message = @"Please enter a username and password";
+        }else if([result isEqualToString:@"NeedToken"]){
+            message = @"Username/Password invalid";
+        }else{
+            message = @"Username and/or Password invalid";
+        }
+    }
+    else{
+        NSRange uploadRange = [result rangeOfString:@"empty-file"];
+        if (uploadRange.location != NSNotFound) {
+            message = @"No file submitted";
+        }
+        else{
+            message = @"Error with the image";
+        }
+    }
+
+    UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:errorType message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [myAlertView performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
+}
+
+- (IBAction)viewUploadedFile:(id)sender
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://test.wikipedia.org/wiki/File:%@", uploadFileName]]];
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
@@ -128,7 +211,9 @@
      */
     NSLog(@"picked: %@", info);
     self.imagePreview.image = info[UIImagePickerControllerOriginalImage];
+    theAssetURL = info[UIImagePickerControllerReferenceURL];
     [self dismissViewControllerAnimated:YES completion:nil];
+    [self getAsset];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
